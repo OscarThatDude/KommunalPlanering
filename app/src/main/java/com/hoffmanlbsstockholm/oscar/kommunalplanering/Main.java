dde6165740e98709304b91b6c555f23e54ff3aff
oscarthatdude <oscar.hoffman@elev.lbs.se>
2016-02-26 08:59:24 +0100

    Alpah 2.0

--- app/src/main/java/com/hoffmanlbsstockholm/oscar/kommunalplanering/Main.java
+++ app/src/main/java/com/hoffmanlbsstockholm/oscar/kommunalplanering/Main.java
@@ -2,6 +2,7 @@ package com.hoffmanlbsstockholm.oscar.kommunalplanering;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Main extends AppCompatActivity {
 
@@ -10,4 +11,11 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void kontakta_databas() {
+
+        EditText input_1= (EditText)findViewById(R.id.input_1);
+        String input_dataget= input_1.getText().toString();
+    }
 }
+
